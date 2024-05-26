9f6c952db30e557ff37c0aa746e67258b76e9f11
mokaddemhicham <mokaddem.ensa@uhp.ac.ma>
2024-05-26 20:53:52 +0100

    Update requirements and Dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, url_for, redirect, render_template, jsonify
+from flask import Flask, request, url_for, redirect, render_template, jsonify
 
 import pandas as pd
 import pickle
@@ -12,7 +12,7 @@ loaded_model = tf.keras.models.load_model("mo.tf")
 
 loaded_model.summary()
 sample_text = (
-	'''The movie by ENSA berrechid was so good and the animation are so dope.
+    '''The movie by ENSA berrechid was so good and the animation are so dope.
 	I would recommend my friends to watch it.'''
 )
 predictions = loaded_model.predict(np.array([sample_text]))
@@ -20,10 +20,9 @@ print(type(predictions))
 print(*predictions[0])
 
 if predictions[0] > 0:
-	print('The review is positive')
+    print('The review is positive')
 else:
-	print('The review is negative')
- 
+    print('The review is negative')
 
 # Initalize the Flask app
 app = Flask(__name__)
@@ -33,10 +32,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-   
-        
     data = request.json
     text = data.get('text')
     sample = (text)
@@ -45,6 +43,7 @@ def predict():
     print(prediction[0])
     response = {'prediction': float(prediction[0][0])}
     return jsonify(response)
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)

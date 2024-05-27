eacf7e0d3a684afe5beddf839ba0c9468a15da7d
mokaddemhicham <mokaddem.ensa@uhp.ac.ma>
2024-05-27 09:28:24 +0100

    clean the app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template, jsonify
 import numpy as np
 import tensorflow as tf
 import os
+from waitress import serve
 
 # Load the saved model
 loaded_model = tf.keras.models.load_model("mo.tf")
@@ -30,7 +31,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    from waitress import serve
-
     port = int(os.environ.get('PORT', 1000))
     serve(app, host='0.0.0.0', port=port)
